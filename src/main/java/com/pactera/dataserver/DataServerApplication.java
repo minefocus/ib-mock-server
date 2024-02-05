69373928a972212098d135846d099f4d9b4bf506
崔鸿涛 <hongtao.cui@pactera.com>
2024-02-05 15:12:45 +0800

    feat:无用代码删除

--- src/main/java/com/pactera/dataserver/DataServerApplication.java
+++ src/main/java/com/pactera/dataserver/DataServerApplication.java
@@ -2,8 +2,9 @@ package com.pactera.dataserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DataServerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/pactera/dataserver/core/sbcf/req/SbcfRootRequest.java
+++
@@ -1,9 +0,0 @@
-package com.pactera.dataserver.core.sbcf.req;
-
-import com.pactera.dataserver.core.sbcf.UtilDoc;
-import lombok.Data;
-
-@Data
-public class SbcfRootRequest<D extends UtilDoc> {
-    private SbcfRequest<D> request;
-}
